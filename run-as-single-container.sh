bb56a54f10050920ce171a943a09824424cad154
Rachmad Setiawan <sheenidgs137@gmail.com>
2022-09-13 20:11:36 +0700

    fix(sh): using absolute path instead of relative path

--- run-as-single-container.sh
+++ run-as-single-container.sh
@@ -2,9 +2,9 @@ docker run \
   -dit \
   -p 127.0.0.1:8200:8200 \
   --restart=always \
-  -v ./volumes/logs:/vault/logs \
-  -v ./volumes/file:/vault/file \
-  -v ./volumes/config:/vault/config \
+  -v $(pwd)/volumes/logs:/vault/logs \
+  -v $(pwd)/volumes/file:/vault/file \
+  -v $(pwd)/volumes/config:/vault/config \
   --cap-add="IPC_LOCK" \
   --entrypoint="vault server -config=/vault/config/vault.json" \
   --name vault \
